36ff7c56e3024f1e1f8531fb3d485f49bc80625f
Abdulhakeem Ahmed <keemahmed99@gmail.com>
2026-02-04 14:29:34 +0100

    zustand 001

---
+++ recipe-sharing-app/src/App.jsx
@@ -0,0 +1,16 @@
+import { useState } from 'react';
+import './App.css'
+import RecipeList from './components/RecipeList'
+import AddRecipeForm from './components/AddRecipeForm'
+function App(){
+    return (
+      <div>
+      <RecipeList />
+      <AddRecipeForm />
+      </div>
+    )
+} 
+
+
+
+export default App

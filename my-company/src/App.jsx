1a4f21b394bc997ff2842d44fb8a21ebd5003ad8
Abdulhakeem Ahmed <keemahmed99@gmail.com>
2026-02-04 10:52:14 +0100

    done task 3

---
+++ my-company/src/App.jsx
@@ -0,0 +1,24 @@
+import { useState } from 'react'
+import './App.css'
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Home from "./components/Home.jsx";
+import About from "./components/About.jsx";
+import Services from "./components/Services.jsx";
+import Contact from "./components/Contact.jsx";
+
+function App() {
+  return (
+    <BrowserRouter>
+      <Navbar/>
+      <Routes>
+        <Route path="/home" element={<Home />} />
+        <Route path="/about" element={<About />} />
+        <Route path="/services" element={<Services />} />
+        <Route path="/contact" element={<Contact />} />
+      </Routes>
+    </BrowserRouter>
+  );
+}
+
+export default App;
+

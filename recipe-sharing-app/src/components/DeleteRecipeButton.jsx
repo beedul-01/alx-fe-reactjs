125d023c5b90ab770b7f46780e43e28db9723733
Abdulhakeem Ahmed <keemahmed99@gmail.com>
2026-02-04 15:15:23 +0100

    .....

---
+++ recipe-sharing-app/src/components/DeleteRecipeButton.jsx
@@ -0,0 +1,22 @@
+import { useNavigate } from "react-router-dom";
+import useRecipeStore from "../store/useRecipeStore";
+
+function DeleteRecipeButton({ recipeId }) {
+  const deleteRecipe = useRecipeStore(
+    (state) => state.deleteRecipe
+  );
+  const navigate = useNavigate();
+
+  const handleDelete = () => {
+    deleteRecipe(recipeId);
+    navigate("/");
+  };
+
+  return (
+    <button onClick={handleDelete}>
+      Delete Recipe
+    </button>
+  );
+}
+
+export default DeleteRecipeButton;
---
+++ recipe-sharing-app/src/components/RecipeDetails.jsx
@@ -0,0 +1,29 @@
+import { useParams } from "react-router-dom";
+import useRecipeStore from "../store/useRecipeStore";
+import DeleteRecipeButton from "../components/DeleteRecipeButton";
+import EditRecipeForm from "../components/EditRecipeForm";
+
+function RecipeDetails() {
+  const { id } = useParams();
+  const recipes = useRecipeStore((state) => state.recipes);
+
+  const recipe = recipes.find(
+    (recipe) => recipe.id === Number(id)
+  );
+
+  if (!recipe) {
+    return <p>Recipe not found</p>;
+  }
+
+  return (
+    <div>
+      <h2>{recipe.title}</h2>
+      <p>{recipe.description}</p>
+
+      <EditRecipeForm recipe={recipe} />
+      <DeleteRecipeButton recipeId={recipe.id} />
+    </div>
+  );
+}
+
+export default RecipeDetails;

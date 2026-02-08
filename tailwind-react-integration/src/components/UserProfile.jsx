9242a85b07619a8467f138738a6e2a5b93396621
Abdulhakeem Ahmed <keemahmed99@gmail.com>
2026-02-08 23:39:23 +0100

    done 003

--- tailwind-react-integration/src/components/UserProfile.jsx
+++ tailwind-react-integration/src/components/UserProfile.jsx
@@ -3,29 +3,29 @@ function UserProfile() {
     <div
       className="
         bg-gray-100
-        p-4 md:p-8
-        max-w-xs md:max-w-sm
         mx-auto my-20
         rounded-lg
         shadow-lg
         text-center
+        p-4 md:p-8
+        max-w-xs md:max-w-sm
       "
     >
       <img
         src="https://via.placeholder.com/150"
         alt="User"
         className="
+          mx-auto
           rounded-full
           w-24 h-24 md:w-36 md:h-36
-          mx-auto
         "
       />
 
       <h1
         className="
-          text-lg md:text-xl
-          text-blue-800
           my-4
+          text-blue-800
+          text-lg md:text-xl
           font-semibold
         "
       >
@@ -34,8 +34,8 @@ function UserProfile() {
 
       <p
         className="
-          text-sm md:text-base
           text-gray-600
+          text-sm md:text-base
         "
       >
         Developer at Example Co. Loves to write code and explore new technologies.
@@ -45,3 +45,4 @@ function UserProfile() {
 }
 
 export default UserProfile;
+
